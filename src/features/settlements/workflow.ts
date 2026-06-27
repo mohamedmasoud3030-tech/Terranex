902da36f38d0e857876bb38cc6e8bc2f234357ce
mohamedmasoud3030-tech <mohamedmasoud3030@gmail.com>
2026-06-28 02:27:06 +0400

    feat: expose multi-obligation settlement workflow

--- src/features/settlements/workflow.ts
+++ src/features/settlements/workflow.ts
@@ -1,6 +1,9 @@
 export {
   getSettlementReceipt,
   recordSettlement,
+  recordSettlementWithAllocations,
   reverseSettlement,
   type RecordSettlementInput,
+  type RecordSettlementWithAllocationsInput,
+  type SettlementAllocationPlan,
 } from './posting';
